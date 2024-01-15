108c5430c80dd90e1ec76c339db135d745607f93
Martin Ruthaivilavan <mmvilavan@gmail.com>
2024-01-14 19:55:38 -0600

    update css, js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-const resultContainer = document.getElementById('table-results')
-// global variable
-// let currentSeason = season
-// let currentRound = round
+const resultContainer = document.getElementById('table-results');
+//  global variable
+//  let currentSeason = season
+//  let currentRound = round
+async function getFone(season, round) {
+    const response = await fetch(`http://ergast.com/api/f1/${season}/${round}/driverstandings.json`);
+    const data = await response.json();
 
-async function getFone(season,round) {
-
-    const response = await fetch(`http://ergast.com/api/f1/${season}/${round}/driverstandings.json`)
-    const data = await response.json()
-    
-    resultContainer.innerHTML = ``
 // loop through length in the the table lenght of array. or for OF loop. 
-    for (let i = 0; i < 7; i++) {
-        resultContainer.innerHTML += `
-    
+    resultContainer.innerHTML = '';
 
+
+    for (let i = 0; i < 10; i++) {
+        resultContainer.innerHTML += `
             <tr class="driverRow">
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position}</td>
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName} ${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName}</td>
@@ -21,33 +19,33 @@ async function getFone(season,round) {
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality}</td>
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points}</td>
             </tr> 
-     
-
-        `  
+        `;
     }
 
+    // display the user search via textContent
+    const displaySeasonId = document.getElementById('displaySeasonId');
+    const displayRoundId = document.getElementById('displayRoundId');
+
+    displaySeasonId.textContent = "Season Year: " + season;
+    displayRoundId.textContent = "Round: " + round;
 }
-   
-const formulaSearchForm = document.getElementById('formulaOneSearch')
+
+// function to submit results to apit via getFone 
+const formulaSearchForm = document.getElementById('formulaOneSearch');
 
 formulaSearchForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    const idInputOne = formulaSearchForm.querySelector('#seasonId')
-    const idInputTwo = formulaSearchForm.querySelector('#roundId')
-    getFone(idInputOne.value, idInputTwo.value)
-    idInputOne.value = ''
-    idInputTwo.value = ''
-    
+    e.preventDefault();
+    const idInputOne = formulaSearchForm.querySelector('#seasonId').value;
+    const idInputTwo = formulaSearchForm.querySelector('#roundId').value;
+    getFone(idInputOne, idInputTwo);
+    idInputOne.value = '';
+    idInputTwo.value = '';
+});
 
-})
-
-const reloadform = document.querySelector('#reload')
+const reloadform = document.querySelector('#reload');
 reloadform.addEventListener('click', () => {
     window.location.reload(true);
-})
-
-
-
+});
 
 
 
